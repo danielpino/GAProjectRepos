eb1b2525ecc24e4f565638232e078d32e8d9f4af
Daniel Pino <pino.danielj@gmail.com>
2017-11-14 22:47:21 -0500

    Express works with routing.

--- 2.express/server.js
+++ 2.express/server.js
@@ -2,8 +2,19 @@ var express = require('express'); // Loading the express module on our server
 
 var app = express(); // Creates a new instance of express on our server
 
-app.get('/', function(req, res) { 
-  // when a request comes in at localhost:3000, it will respond 
+app.get("/", function(req, res){
+  // display 'Hello World!'
+  res.send('Hello World!');
+});
+
+app.get("/greeting", function(req, res){
+  // display 'Hello World!'
+  res.send('Hey, WDI 12!');
+});
+
+app.get("/rihanna", function(req, res){
+  // display 'Hello World!'
+  res.send('Work work work work work');
 });
 
 var port = process.env.PORT || 3000; // tells the server where to listen for requests
