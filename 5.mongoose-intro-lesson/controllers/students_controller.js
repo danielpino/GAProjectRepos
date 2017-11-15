d02fdfcc723e9c2bcc5a156245b16a9ec102a9a4
Daniel Pino <pino.danielj@gmail.com>
2017-11-15 18:00:01 -0500

    Done with lesson #7 (and combined with lesson #8).  Updates, adds, modifying nested objects. Pretty much anything you can do with data.

--- 5.mongoose-intro-lesson/controllers/students_controller.js
+++ 5.mongoose-intro-lesson/controllers/students_controller.js
@@ -9,7 +9,7 @@ const ProjectModel = Schema.ProjectModel
 
 router.use(bodyParser.urlencoded({
     extended: true
-  }));
+}));
 router.use(methodOverride('_method'));
 
 router.get('/', (req, res) => {
@@ -25,15 +25,18 @@ router.get('/', (req, res) => {
         })
 })
 
-router.get('/:studentId', (req, res) => {
-    StudentModel.findById(req.params.studentId)
-        .then((student) => {
-            console.log(student);
-            res.render('show', {
-                student
-            })
-        })
-})
+router.post('/', function (req, res) {
+    var student = new StudentModel({
+        name: req.body.name
+    });
+    student.save(function (err, student) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(student);
+        res.redirect('/students');
+    });
+});
 
 router.put('/:studentId', (req, res) => {
     const updatedStudent = req.body;
@@ -45,21 +48,163 @@ router.put('/:studentId', (req, res) => {
         })
         .then((student) => {
             console.log(`${student.name} updated!`);
-            res.redirect(`/students/${studentId}`);
+            res.redirect(`/students/${req.params.studentId}`);
         })
         .catch((error) => {
             console.log(error)
         })
 })
 
-router.get('/:studentId/edit', (req, res) => {
+router.delete('/:studentId', (req, res) => {
+    const studentId = req.params.studentId
+    console.log(`Deleting student with ID: ${studentId}`)
+
+    StudentModel.findByIdAndRemove(studentId)
+        .then(() => {
+            res.redirect('/students')
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+router.get('/:studentId', (req, res) => {
     StudentModel.findById(req.params.studentId)
         .then((student) => {
-            res.render('edit', {
+            console.log(`Getting student with ID: `, student.id);
+            res.render('show_student', {
                 student
             })
         })
 })
 
+router.get('/:studentId/edit', (req, res) => {
+    StudentModel.findById(req.params.studentId)
+        .then((student) => {
+            res.render('edit_student', {
+                student
+            })
+        })
+})
+
+router.get('/:studentId/projects', (req, res) => {
+    StudentModel.findById(req.params.studentId)
+        .then((student) => {
+            res.render('show_student', {
+                student
+            })
+        })
+})
+
+router.put('/:studentId/projects', (req, res) => {
+    const studentId = req.params.studentId;
+    const newProject = req.body;
+
+    StudentModel.findById(studentId)
+        .then((student) => {
+            student.projects.push(newProject);
+            StudentModel.findByIdAndUpdate(studentId, student, {
+                    new: true
+                })
+                .then((student) => {
+                    console.log(`${student.name} updated!`);
+                    res.redirect(`/students/${studentId}`);
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        })
+})
+
+router.put('/:studentId/projects/:projectId', (req, res) => {
+    StudentModel.findById(studentId)
+        .then((student) => {
+            var oldProject = student.projects.id(projectId);
+            oldProject.title = updatedProject.title;
+            oldProject.unit = updatedProject.unit;
+            StudentModel.findByIdAndUpdate(studentId, student, {
+                    new: true
+                })
+                .then((student) => {
+                    console.log(`${student.name} updated!`);
+                    res.redirect(`/students/${studentId}`);
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        })
+    res.redirect(`${projectId}`);
+})
+
+router.get('/:studentId/projects/add', (req, res) => {
+    StudentModel.findById(req.params.studentId)
+        .then((student) => {
+            res.render('add_project', {
+                student
+            })
+        })
+})
+
+router.get('/:studentId/projects/:projectId', (req, res) => {
+    StudentModel.findById(req.params.studentId)
+        .then((student) => {
+            var project = student.projects.find(function (project) {
+                return project._id == req.params.projectId
+            })
+            res.render('show_project', {
+                project,
+                student
+            })
+        })
+})
+
+router.get('/:studentId/projects/:projectId/edit', (req, res) => {
+    StudentModel.findById(req.params.studentId)
+        .then((student) => {
+            var project = student.projects.id(req.params.projectId);
+            res.render('edit_project', {
+                project,
+                student
+            })
+        })
+})
+
+router.put('/:studentId/projects/:projectId', (req, res) => {
+    const studentId = req.params.studentId;
+    const projectId = req.params.projectId;
+
+    const updatedProject = req.body;
+    console.log(req.body);
+
+    StudentModel.findByIdAndUpdate(studentId, {
+        $push: {
+            projects: {
+                updatedProject
+            }
+        }
+    }).exec(function (err, item) {
+        if (err) console.log(err);
+        res.redirect(`/students/${studentId}`);
+    })
+})
+
+router.delete('/:studentId/projects/:projectId', (req, res) => {
+    const studentId = req.params.studentId;
+    const projectId = req.params.projectId;
+
+    const updatedProject = req.body;
+    console.log(req.body);
+
+    StudentModel.findByIdAndUpdate(studentId, {
+        $pull: {
+            projects: {
+                _id: projectId
+            }
+        }
+    }).exec(function (err, item) {
+        if (err) console.log(err);
+        res.redirect(`/students/${studentId}`);
+    })
+})
 
 module.exports = router;
