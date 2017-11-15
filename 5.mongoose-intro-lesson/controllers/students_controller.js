f1926d20d22dd7c7d16ae30398baadf91bd483ee
Daniel Pino <pino.danielj@gmail.com>
2017-11-15 15:09:46 -0500

    struggling with "update"

--- 5.mongoose-intro-lesson/controllers/students_controller.js
+++ 5.mongoose-intro-lesson/controllers/students_controller.js
@@ -1,31 +1,65 @@
 const express = require('express')
 const router = express.Router()
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 
 const Schema = require("../db/schema.js")
 const StudentModel = Schema.StudentModel
 const ProjectModel = Schema.ProjectModel
 
-router.get('/', (request, response) => {
-  StudentModel.find({})
-    .then((students) => {
-      response.render('index', { 
-        students: students
-      })
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+router.use(bodyParser.urlencoded({
+    extended: true
+  }));
+router.use(methodOverride('_method'));
+
+router.get('/', (req, res) => {
+    StudentModel.find({})
+        .then((students) => {
+            console.log(students);
+            res.render('index', {
+                students
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 })
 
-router.get('/:studentId', (request, response) => {
-    const studentId = request.params.studentId
-  
-    StudentModel.findById(studentId)
-      .then((student) => {
-        response.render('show', {
-          student: student
+router.get('/:studentId', (req, res) => {
+    StudentModel.findById(req.params.studentId)
+        .then((student) => {
+            console.log(student);
+            res.render('show', {
+                student
+            })
         })
-      })
-  })
+})
+
+router.put('/:studentId', (req, res) => {
+    const updatedStudent = req.body;
+
+    console.log(req.body);
+
+    StudentModel.findByIdAndUpdate(req.params.studentId, updatedStudent, {
+            new: true
+        })
+        .then((student) => {
+            console.log(`${student.name} updated!`);
+            res.redirect(`/students/${studentId}`);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+router.get('/:studentId/edit', (req, res) => {
+    StudentModel.findById(req.params.studentId)
+        .then((student) => {
+            res.render('edit', {
+                student
+            })
+        })
+})
+
 
 module.exports = router;
--- 5.mongoose-intro-lesson/server.js
+++ 5.mongoose-intro-lesson/server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express(); // install express on our server
 var router = express.Router();
-// var bodyParser = require('body-parser');
-// var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var studentController = require('./controllers/students_controller.js');
 
 var port = process.env.PORT || 3000; // tells the server where to listen for requests
@@ -13,7 +13,7 @@ app.listen(port, function () {
 app.set("view engine", "hbs");
 
 app.use('/students', studentController);
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
-// app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(methodOverride('_method'));
