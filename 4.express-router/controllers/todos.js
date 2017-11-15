8b81cde853cbc9560004cea85463b66aaa45107d
Daniel Pino <pino.danielj@gmail.com>
2017-11-15 12:26:16 -0500

    Lesson #6 complete. method-override, put, update, delete

--- 4.express-router/controllers/todos.js
+++ 4.express-router/controllers/todos.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 var hbs = require('hbs');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var data = require('../data.js');
 
 router.use(bodyParser.urlencoded({
   extended: true
 }));
+router.use(methodOverride('_method'));
 
 router.get('/', function (req, res) {
   res.render('index', {
@@ -14,7 +16,34 @@ router.get('/', function (req, res) {
   });
 })
 
-router.post('/', function(req, res){
+router.delete('/:id', function (req, res) {
+  data.seededTodos.splice(req.params.id, 1); // remove the item from the array
+
+  res.redirect('/todos'); // redirect back to the index route
+});
+
+router.put('/:id', function(req, res) {
+  var todoToEdit = data.seededTodos[req.params.id];
+
+  todoToEdit.description = req.body.description;
+  todoToEdit.location = req.body.location;
+  todoToEdit.urgent = req.body.urgent;
+
+  res.redirect('/todos');
+})
+
+router.get('/:id/edit', function (req, res) {
+  res.render('edit', {
+    todo: {
+      id: req.params.id,
+      description: data.seededTodos[req.params.id].description,
+      location: data.seededTodos[req.params.id].location,
+      urgent: data.seededTodos[req.params.id].urgent,
+    }
+  });
+});
+
+router.post('/', function (req, res) {
   console.log("POSTING")
   data.seededTodos.push(req.body);
   res.redirect('/todos');
--- 4.express-router/server.js
+++ 4.express-router/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express(); // install express on our server
 var router = express.Router();
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var todosController = require('./controllers/todos.js');
 
 var port = process.env.PORT || 3000; // tells the server where to listen for requests
@@ -15,3 +16,4 @@ app.use('/todos', todosController);
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(methodOverride('_method'));
