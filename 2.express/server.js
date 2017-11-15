885b6a7743b1e3ca209b75900e2511a245b20a55
Daniel Pino <pino.danielj@gmail.com>
2017-11-14 23:36:10 -0500

    End lesson 2. Express with routing, query params, and request params.

--- 2.express/server.js
+++ 2.express/server.js
@@ -33,12 +33,64 @@ app.get("/sightings", function (req, res) {
 
 app.get("/bigfoot", function (req, res) {
   console.log(req.params);
-  if (req.query.blurry){
+  if (req.query.blurry) {
     res.send(`It's not the photographer's fault. Bigfoot is blurry, and that's extra scary to me. There's a large, out-of-focus monster roaming the countryside. Run! He's fuzzy! Get out of there!`);
   } else {
     res.send(`A group of researchers have amassed evidence that the legendary Bigfoot is real, ABC News reported, with the scientists presenting reams of evidence.`)
   }
-  
+});
+
+app.get('/hello/:name', function (req, res) {
+  res.send({
+    params: req.params,
+    queries: req.query
+  });
+});
+
+app.get('/favorite/:noun', function (req, res) {
+  res.send(`I have a favorite ${req.params.noun}, it is ${req.query.noun}.`);
+});
+
+app.get('/add', function (req, res) {
+  var answer = parseInt(req.query.num1) + parseInt(req.query.num2);
+  res.send(`${answer}`);
+});
+
+app.get('/subtract', function (req, res) {
+  var answer = parseInt(req.query.num1) - parseInt(req.query.num2);
+  res.send(`${answer}`);
+});
+
+app.get('/multiply', function (req, res) {
+  var answer = parseInt(req.query.num1) * parseInt(req.query.num2);
+  res.send(`${answer}`);
+});
+
+app.get('/divide', function (req, res) {
+  var answer = parseInt(req.query.num1) / parseInt(req.query.num2);
+  res.send(`${answer}`);
+});
+
+app.get('/math/:operator', function (req, res) {
+  console.log(req.params);
+  console.log(req.query);
+  var numQuery = `num1=${req.query.num1}&num2=${req.query.num2}`;
+  switch (req.params.operator) {
+    case 'add':
+      res.redirect(`/add?${numQuery}`);
+      break;
+    case 'subtract':
+      res.redirect(`/subtract?${numQuery}`);
+      break;
+    case 'divide':
+      res.redirect(`/divide?${numQuery}`);
+      break;
+    case 'multiply':
+      res.redirect(`/multiply?${numQuery}`);
+      break;
+  }
+
+
 });
 
 app.get("/:name", function (req, res) {
