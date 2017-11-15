1393d5bb4687af0302b0922e54b3ead299a9c0f3
Daniel Pino <pino.danielj@gmail.com>
2017-11-15 14:28:59 -0500

    lesson #7 cleanup.

--- 5.mongoose-intro-lesson/db/schema.js
+++ 5.mongoose-intro-lesson/db/schema.js
@@ -34,40 +34,6 @@ var StudentSchema = new Schema({
 var ProjectModel = mongoose.model("Project", ProjectSchema);
 var StudentModel = mongoose.model("Student", StudentSchema);
 
-// StudentModel.create({
-//         name: 'Frankie Q.',
-//         age: 31
-//     })
-//     .then(function (student) {
-//         console.log(student);
-//     })
-//     .catch((error) => {
-//         console.log(error)
-//     })
-
-// var anna = new StudentModel({
-//     name: "Anna",
-//     age: 28
-// });
-// var project1 = new ProjectModel({
-//     title: "memory gamesss",
-//     unit: "JS"
-// });
-
-// Now we add that project to a student's collection / array of projects.
-// anna.projects.push(project1);
-
-// In order to save that project to the student, we need to call `.save()` on the student -- not the project.
-// anna.save()
-//     .then((anna) => {
-//         console.log(anna)
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     })
-
-
-// Disconnect from database
 // db.close();
 
 module.exports = {
